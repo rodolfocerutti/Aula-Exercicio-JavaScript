076227a7d09cc60cff59c36df9542ba6c7b1f501
rodolfocerutti <42013617+rodolfocerutti@users.noreply.github.com>
2022-05-23 20:48:49 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,10 @@
+//Exercicio
+
+console.log('Meu nome é "Rodolfo Cerutti". Estou aprendendo JavaScript às 20:30 da noite.');
+console.log(`Hoje eu resolvi 80 problemas, o "Carlos" me ajudou a resolver os problemas.`);
+console.log('Meu pai é o "Jose Augusto" o RG dele é 123456789.');
+console.log("Estou praticando 'JavaScript' e a entender o console.log. Essa é a 4 tentativa");
+
+//Resolução do exercicio
+console.log('Meu nome é "Rodolfo Cerutti". Estou aprendendo JavaScript às', 20, 'da noite');
+console.log('Este', 'É', 'Um', 1000);
---
+++ index.js
@@ -0,0 +1,21 @@
+/*•console.log = mostra o que esta no console
+  •Posso colocar qualquer informação
+  •Sempre colocar '' qual o valor for literal
+  •sempre quebra a linha quando usa mais de console.log
+  •aspas simples '' não é exibida no console
+  •A aspas simples so será exibida se ela estiver envolvida em aspastas duplas "" o mesmo é válido para aspas duplas
+  •Dentro da crase eu consigo usar aspas simples e duplas
+  •Dentro das aspas duplas, simples e crase são chamadas de strings
+  •String = Texto
+  •Numero não precisam de aspas
+  •Numeros não quebram a linha
+  •Console.log não é exibida fora do console*/
+
+console.log('Rodolfo Cerutti'); 
+console.log("Rodolfo Cerutti"); 
+console.log(`Rodolfo Cerutti`);
+console.log("'Rodolfo Cerutti'");
+console.log('"Rodolfo Cerutti"');
+console.log(`'Rodolfo Cerutti' "Rodolfo Cerutti"`);
+
+console.log(123456, 15.85, 'Rodolfo Cerutti novamente');
